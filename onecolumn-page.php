3d60ad434dde29e4ef66db8f9d9b719031932f95
Chia Rhen Huei <rhchia@gmail.com>
2012-03-11 11:12:23 +0800

    Fixed template missing from page attributes

--- onecolumn-page.php
+++ onecolumn-page.php
@@ -1,4 +1,18 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * Template Name: One column, no sidebar
+ *
+ * A custom page template without sidebar.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers 3.0
+ */
+
+get_header(); ?>
 	<div class="grid_12">
 		<div class="one_pad">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
